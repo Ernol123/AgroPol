ecc3fe77e9aed510a28d2a46565f53503b86ab38
Kacper Jama <pt21jamak@zsi.kielce.pl>
2023-09-13 14:38:28 +0200

    sa

--- index.php
+++ index.php
@@ -4,10 +4,29 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Agropol | Strona Główna</title>
+    <link rel="icon" href="./github/logo.jpg" type="image/jpg">
+    <link rel="stylesheet" href="./css/style.css">
 </head>
 
 <body>
+<nav>
+    <div class="logo">
+        AGRO<span>POL</span>
+    </div>
+    <div class="nav_items">
+        <a href="" class="active">Strona Główna</a>
+        <a href="">Lorem</a>
+        <a href="">Lorem</a>
+        <a href="">Lorem</a>
+        <a href="">Lorem</a>
+    </div>
+    <div class="login">
+        <a href="">Logowanie</a>
+        <a href="">Rejestracja</a>
+    </div>
+</nav>
+<div class="border"></div>
 
 </body>
 
