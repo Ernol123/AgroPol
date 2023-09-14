84bf9e336f2b168fef9bf55fc683400ae378d196
Ernol <iwanernest29@gmail.com>
2023-09-14 21:58:08 +0200

    form

---
+++ php/add_form.php
@@ -0,0 +1,21 @@
+<?php
+if (!empty($_FILES['myFile'])) {
+}
+$db = new PDO('mysql:host=localhost; dbname=agropol', 'root', '');
+try {
+    $stmt = $db->prepare("INSERT INTO test VALUE ('" . addslashes(file_get_contents($_FILES['myFile']['tmp_name'])) . "')");
+    $stmt->execute();
+} catch (Exception $exc) {
+    exit('Error: ' . $exc->getMessage());
+}
+
+try {
+    $stmt = $db->prepare("SELECT file FROM test");
+    $stmt->execute();
+} catch (Exception $exc) {
+    exit('Error: ' . $exc->getMessage());
+}
+foreach ($stmt->fetchAll() as $d) {
+    $photo = '<img src="data:image/jpeg;base64,' . base64_encode($d['file']) . '"/>';
+}
+echo $photo;
---
+++ script/form.js
@@ -0,0 +1,47 @@
+let i = 0;
+let j = 0;
+function add() {
+	const xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function () {
+		if (xhr.readyState == 4 && xhr.status == 200) {
+			document.getElementById('error').innerHTML = xhr.responseText;
+		}
+	};
+	xhttp.open('GET', './add_form.php', true);
+	xhttp.send();
+
+	// '" . addslashes(file_get_contents($_FILES['zdjecie']['tmp_name'])) . "'
+}
+function sendFile() {
+	const xhr = new XMLHttpRequest();
+	const fd = new FormData();
+	var file = document.getElementById('photo_1').files[0];
+	console.log(file[0]);
+
+	xhr.open('POST', './php/add_form.php', true);
+	xhr.onreadystatechange = function () {};
+	fd.append('myFile', file);
+	xhr.send(fd);
+}
+
+function addSpecification() {
+	i++;
+	const specifications = document.getElementsByClassName('specification');
+	let clone = specifications[0].cloneNode(true);
+	clone.style.display = 'block';
+	for (let element of clone.children) {
+		element.id += i;
+	}
+	document.getElementById('specification').appendChild(clone);
+}
+
+function addEngine() {
+	j++;
+	const engines = document.getElementsByClassName('engine');
+	let clone = engines[0].cloneNode(true);
+	clone.style.display = 'block';
+	for (let element of clone.children) {
+		element.id += i;
+	}
+	document.getElementById('engine').appendChild(clone);
+}
