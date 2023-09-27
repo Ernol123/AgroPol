a390671f69f2b6ed5e28cc54405b635a3d91cfc9
Ernest Iwan <pt21iwane@zsi.kielce.pl>
2023-09-27 14:44:56 +0200

    fix

--- php/add_form.php
+++ php/add_form.php
@@ -10,7 +10,31 @@
 //     exit('Error: ' . $exc->getMessage());
 // }
 
-print_r($_POST);
+foreach($_POST as $key => $val){
+    // echo "$key = $val";
+    if(str_starts_with($key,'photo') && empty($valu)){
+        echo 'Dodoaj wszystkie zdjęcia';
+        break;
+    } else if($key == 'category' && empty($val)){
+        echo 'Wybierz kategorię';
+        break;
+    } else if($key == 'mark' && empty($val)){
+        echo 'Podaj markę';
+        break;
+    } else if($key == 'model' && $_POST['category'] != 'Nawozy' && $_POST['category'] != 'Rośliny' && empty($val)){
+        echo 'Podaj model';
+        break;
+    } else if($key == 'workingWidth' && $_POST['category'] != 'Traktory' && $_POST['category'] != 'Przyczepa' && $_POST['category'] != 'Paszowozy' && $_POST['category'] != 'Nawozy' && $_POST['category'] != 'Rośliny' && empty($val)){
+        echo 'Podaj szerokość roboczą';
+        break;
+    } else if($key == 'fromCountry' && empty($val)){
+        echo 'Podaj kraj pochodzenia';
+        break;
+    } else if($key == 'price' && empty($val)){
+        echo 'Podaj kraj pochodzenia';
+        break;
+    }
+}
 
 // try {
 //     $stmt = $db->prepare("SELECT file FROM test");
